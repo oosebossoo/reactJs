b74f3c424db00cc1ee164acd738158c350ee67b1
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2023-06-11 13:18:44 +0200

    component added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <>
+            <header className="header">
+                <div style={{float: 'left' }}>
+                    Moja strona www
+                </div>
+                <nav>
+                    <div>Home</div>
+                    <div>Galeria</div>
+                    <div>O mnie</div>
+                    <div>Kontakt</div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
